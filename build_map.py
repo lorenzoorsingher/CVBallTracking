f06ab5b15bb68d8f20e57316299793bbbd195f2c
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-06-12 11:56:07 +0200

    move detection_to_points to own function

--- build_map.py
+++ build_map.py
@@ -13,7 +13,53 @@ from sort import *
 from common import set_axes_equal
 
 from camera_controller import CameraController
-from yolotools.sliced_yolo import SlicedYolo
+from yolotools.sliced_yolo import SlicedYOLO
+
+
+def detections_to_point(all_dets):
+    if len(all_dets.keys()) < 2:
+        return None
+
+    trianglated_points = []
+    checked = {}
+    for cam_idx_1, det1 in all_dets.items():
+        for cam_idx_2, det2 in all_dets.items():
+            if cam_idx_1 == cam_idx_2:
+                continue
+            if (cam_idx_2, cam_idx_1) in checked:
+                continue
+            checked[(cam_idx_1, cam_idx_2)] = True
+            cam1 = cams[cam_idx_1]
+            cam2 = cams[cam_idx_2]
+
+            point3d = cam1.triangulate(cam2, det1, det2)
+
+            trianglated_points.append(point3d)
+
+            # print(f"cam{cam_idx_1} -> cam{cam_idx_2} \t {point3d}")
+
+    trianglated_points = np.array(trianglated_points)
+
+    vec1 = torch.tensor(trianglated_points).unsqueeze(0)
+    vec2 = torch.tensor(trianglated_points).unsqueeze(1)
+    distances = torch.norm(vec1 - vec2, dim=2)
+
+    good_points = []
+    for i in range(distances.shape[0]):
+        for j in range(i, distances.shape[1]):
+            if distances[i][j] < 1000 and i != j:
+                # print(f"{distances[i][j]} \t {trianglated_points[i]}")
+                # print(f"{distances[i][j]}\t [{i} {j}]")
+                if i not in good_points:
+                    good_points.append(i)
+
+    good_points = np.array([trianglated_points[i] for i in good_points])
+
+    if len(good_points) == 0:
+        return None
+
+    final_point = np.mean(good_points, axis=0)
+    return final_point
 
 
 positions_path = "data/camera_data/camera_positions.json"
@@ -23,32 +69,6 @@ with open(positions_path, "r") as file:
     positions = data["positions"]
     field_corners = np.array(data["field_corners"]) * 1000
 # Scale the axes equally
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.set_box_aspect([1, 1, 1])
-plt.ion()
-plt.show()
-# Plotting real_corners
-ax.scatter(
-    field_corners[:, 0],
-    field_corners[:, 1],
-    field_corners[:, 2],
-    c="red",
-    label="Real Corners",
-)
-set_axes_equal(ax)
-
-
-def plot_3d_points(points):
-
-    ax.scatter(
-        points[:, 0],
-        points[:, 1],
-        points[:, 2],
-        c="blue",
-        label="Real Corners",
-    )
-    plt.pause(1)
 
 
 dump = []
@@ -94,17 +114,39 @@ while True:
         steps.append([frame_idx, new_step])
         steps.pop(0)
         break
-breakpoint()
 
 
-for step in steps:
-    frm_idx, point = step
+plot_points = []
+for step in steps[500:2000]:
+    frm_idx, all_dets = step
 
-    ax.scatter(
-        point[0],
-        point[1],
-        point[2],
-        c="blue",
-        label="Real Corners",
-    )
-    plt.pause(4)
+    final_point = detections_to_point(all_dets)
+    print(f"FINAL POINT: {final_point}")
+    if final_point is not None:
+        plot_points.append(final_point)
+
+from mpl_toolkits.mplot3d import Axes3D
+
+
+plot_points = np.array(plot_points).T
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.set_box_aspect([1, 1, 1])
+# plt.ion()
+# plt.show()
+# Plotting real_corners
+ax.scatter(
+    field_corners[:, 0],
+    field_corners[:, 1],
+    field_corners[:, 2],
+    c="red",
+    label="Real Corners",
+)
+set_axes_equal(ax)
+
+
+ax.plot(plot_points[0], plot_points[1], plot_points[2])
+ax.scatter(*plot_points.T[0], color="red")
+plt.show()
--- multi_track.py
+++ multi_track.py
@@ -13,128 +13,12 @@ from sort import *
 from common import set_axes_equal
 
 from camera_controller import CameraController
-from yolotools.sliced_yolo import SlicedYolo
+from yolotools.sliced_yolo import SlicedYOLO
 
 
-positions_path = "data/camera_data/camera_positions.json"
-with open(positions_path, "r") as file:
-
-    data = json.load(file)
-    positions = data["positions"]
-    field_corners = np.array(data["field_corners"]) * 1000
-# Scale the axes equally
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.set_box_aspect([1, 1, 1])
-plt.ion()
-plt.show()
-# Plotting real_corners
-ax.scatter(
-    field_corners[:, 0],
-    field_corners[:, 1],
-    field_corners[:, 2],
-    c="red",
-    label="Real Corners",
-)
-set_axes_equal(ax)
-
-
-def plot_3d_points(points):
-
-    ax.scatter(
-        points[:, 0],
-        points[:, 1],
-        points[:, 2],
-        c="blue",
-        label="Real Corners",
-    )
-    plt.pause(1)
-
-
-model_path = (
-    "/home/lollo/Documents/python/CV/CVBallTracking/runs/detect/train2/weights/best.pt"
-)
-model = YOLO(model_path)
-
-sliced_yolo = SlicedYolo(model_path=model_path, wsize=(640, 640), overlap=(0.8, 0.8))
-
-cam_idxs = [1, 2, 3, 4, 5, 6, 7, 8]
-videos_path = "/home/lollo/Documents/python/CV/CVBallTracking/data/fake_basket"
-video_paths = [f"{videos_path}/out{cam_idx}.mp4" for cam_idx in cam_idxs]
-cams = [CameraController(cam_idx) for cam_idx in cam_idxs]
-caps = [cv.VideoCapture(video_paths[idx]) for idx in range(len(video_paths))]
-# trackers = [Sort() for _ in range(len(cam_idxs))]
-
-
-cv.namedWindow("frame", cv.WINDOW_NORMAL)
-
-curr_cam_idx = 4
-frame_skip = 10
-frame_idx = 200
-
-
-tracked_points = []
-every_det = []
-while True:
-    print(f"FRAME {frame_idx}-------------------------------")
-    all_outs = []
-    all_dets = {}
-    ret = True
-    for curr_cam_idx in range(len(cam_idxs)):
-        cap = caps[curr_cam_idx]
-        cam = cams[curr_cam_idx]
-        # curr_tracker = trackers[curr_cam_idx]
-
-        cap.set(cv.CAP_PROP_POS_FRAMES, frame_idx)
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # uframe = cam.undistort_img(frame)
-        uframe = frame
-
-        out, det = sliced_yolo.predict(uframe)
-
-        if out is not None:
-
-            x, y, w, h, c = out
-            # breakpoint()
-            # detections = np.array([[x, y, x + w, y + h, c]])
-            all_dets[curr_cam_idx] = [x + w // 2, y + h // 2]
-            uframe = cv.rectangle(
-                uframe,
-                (x - w // 2, y - h // 2),
-                (x + w // 2, y + h // 2),
-                (0, 255, 0),
-                8,
-            )
-        else:
-            # detections = np.empty((0, 5))
-            pass
-
-        # track_bbs_ids = curr_tracker.update(detections)
-        # print(f"cam {curr_cam_idx} -> {track_bbs_ids}")
-
-        all_outs.append(cv.resize(uframe, (640, 480)))
-
-    if not ret:
-        frame_idx = 0
-        continue
-    else:
-        frame_idx += 1
-
-    # every_det.append((frame_idx, all_dets))
-
-    # with open(f"dets.txt", "w") as f:
-    #     for entr in every_det:
-    #         fr_idx, dd = entr
-    #         if len(all_dets.keys()) < 2:
-    #             f.write(f"{fr_idx};-1;;\n")
-    #         for key, value in dd.items():
-    #             f.write(f"{fr_idx};{key};{value[0]};{value[1]}\n")
-
+def detections_to_point(all_dets):
     if len(all_dets.keys()) < 2:
-        continue
+        return None
 
     trianglated_points = []
     checked = {}
@@ -172,27 +56,111 @@ while True:
     good_points = np.array([trianglated_points[i] for i in good_points])
 
     if len(good_points) == 0:
-        continue
+        return None
 
     final_point = np.mean(good_points, axis=0)
+    return final_point
+
+
+positions_path = "data/camera_data/camera_positions.json"
+with open(positions_path, "r") as file:
+
+    data = json.load(file)
+    positions = data["positions"]
+    field_corners = np.array(data["field_corners"]) * 1000
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.set_box_aspect([1, 1, 1])
+plt.ion()
+plt.show()
+ax.scatter(
+    field_corners[:, 0],
+    field_corners[:, 1],
+    field_corners[:, 2],
+    c="red",
+    label="Real Corners",
+)
+set_axes_equal(ax)
+
+
+def plot_3d_points(points):
+
+    ax.scatter(
+        points[:, 0],
+        points[:, 1],
+        points[:, 2],
+        c="blue",
+        label="Real Corners",
+    )
+    plt.pause(1)
+
+
+model_path = (
+    "/home/lollo/Documents/python/CV/CVBallTracking/runs/detect/train2/weights/best.pt"
+)
+model = YOLO(model_path)
+
+sliced_yolo = SlicedYOLO(model_path=model_path, wsize=(640, 640), overlap=(0.8, 0.8))
+
+cam_idxs = [1, 2, 3, 4, 5, 6, 7, 8]
+videos_path = "/home/lollo/Documents/python/CV/CVBallTracking/data/fake_basket"
+video_paths = [f"{videos_path}/out{cam_idx}.mp4" for cam_idx in cam_idxs]
+cams = [CameraController(cam_idx) for cam_idx in cam_idxs]
+caps = [cv.VideoCapture(video_paths[idx]) for idx in range(len(video_paths))]
+# trackers = [Sort() for _ in range(len(cam_idxs))]
+
+
+cv.namedWindow("frame", cv.WINDOW_NORMAL)
+
+frame_skip = 10
+frame_idx = 200
+
+
+tracked_points = []
+every_det = []
+while True:
+    print(f"FRAME {frame_idx}-------------------------------")
+    all_frames = []
+    all_dets = {}
+    ret = True
+    for curr_cam_idx in range(len(cam_idxs)):
+        cap = caps[curr_cam_idx]
+        cam = cams[curr_cam_idx]
+        # curr_tracker = trackers[curr_cam_idx]
+
+        cap.set(cv.CAP_PROP_POS_FRAMES, frame_idx)
+        ret, frame = cap.read()
+        if not ret:
+            print("[TRACK] Frame corrupted, exiting...")
+            exit()
+
+        # uframe = cam.undistort_img(frame)
+        uframe = frame
+
+        out, det = sliced_yolo.predict(uframe, viz=True)
+
+        if out is not None:
+            x, y, w, h, c = out
+            all_dets[curr_cam_idx] = [x + w // 2, y + h // 2]
+
+        all_frames.append(cv.resize(uframe, (640, 480)))
+
+    frame_idx += 1
+
+    final_point = detections_to_point(all_dets)
+
+    if final_point is None:
+        continue
+
     print(f"FINAL POINT: {final_point}")
-    # breakpoint()
-    tracked_points.append(final_point)
-    tracked_points_np = np.array(tracked_points)
-    # breakpoint()
-    # frame = sliced_yolo.print_windows(uframe)
 
-    # if frame_idx % 10 == 0:
-    plot_3d_points(tracked_points_np)
+    # tracked_points.append(final_point)
+    # tracked_points_np = np.array(tracked_points)
+    # plot_3d_points(tracked_points_np)
 
-    frame = np.vstack([np.hstack(all_outs[:4]), np.hstack(all_outs[4:])])
+    frame = np.vstack([np.hstack(all_frames[:4]), np.hstack(all_frames[4:])])
     cv.imshow("frame", frame)
-
     k = cv.waitKey(1)
-
-    if k == ord("n"):
-        curr_cam_idx = (curr_cam_idx + 1) % (len(cam_idxs))
-        print(f"SWITCHED TO CAMERA{cam_idxs[curr_cam_idx]}")
     if k == ord("d"):
         print(f"SKIPPING {frame_skip} FRAMES")
         frame_idx += frame_skip
