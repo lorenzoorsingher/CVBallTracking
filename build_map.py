0b97ca3803c1a79b0f0e8a06c9750068f8486417
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-06-12 17:34:58 +0200

    add kalman filter

--- build_map.py
+++ build_map.py
@@ -16,6 +16,37 @@ from camera_controller import CameraController
 from yolotools.sliced_yolo import SlicedYOLO
 
 
+def from_file():
+    dump = []
+    with open("dett.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            frame_idx, cam_idx, x, y = line.strip().split(";")
+            frame_idx = int(frame_idx)
+            cam_idx = int(cam_idx)
+            x = float(x)
+            y = float(y)
+            dump.append([frame_idx, cam_idx, x, y])
+
+    steps = []
+    new_step = {}
+    frame_idx = -1
+    while True:
+
+        step = dump.pop(0)
+        frm_idx, cam_idx, x, y = step
+        if frame_idx != frm_idx:
+            steps.append([frame_idx, new_step])
+            frame_idx = frm_idx
+            new_step = {}
+        new_step[cam_idx] = [x, y]
+        if len(dump) == 0:
+            steps.append([frame_idx, new_step])
+            steps.pop(0)
+            break
+    return steps
+
+
 def detections_to_point(all_dets):
     if len(all_dets.keys()) < 2:
         return None
@@ -62,91 +93,60 @@ def detections_to_point(all_dets):
     return final_point
 
 
-positions_path = "data/camera_data/camera_positions.json"
-with open(positions_path, "r") as file:
+if __name__ == "__main__":
+    positions_path = "data/camera_data/camera_positions.json"
+    with open(positions_path, "r") as file:
 
-    data = json.load(file)
-    positions = data["positions"]
-    field_corners = np.array(data["field_corners"]) * 1000
-# Scale the axes equally
+        data = json.load(file)
+        positions = data["positions"]
+        field_corners = np.array(data["field_corners"]) * 1000
+    # Scale the axes equally
 
+    ################################
 
-dump = []
-with open("dett.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        frame_idx, cam_idx, x, y = line.strip().split(";")
-        frame_idx = int(frame_idx)
-        cam_idx = int(cam_idx)
-        x = float(x)
-        y = float(y)
-        dump.append([frame_idx, cam_idx, x, y])
-################################
+    cam_idxs = [1, 2, 3, 4, 5, 6, 7, 8]
+    videos_path = "/home/lollo/Documents/python/CV/CVBallTracking/data/fake_basket"
+    video_paths = [f"{videos_path}/out{cam_idx}.mp4" for cam_idx in cam_idxs]
+    cams = [CameraController(cam_idx) for cam_idx in cam_idxs]
+    caps = [cv.VideoCapture(video_paths[idx]) for idx in range(len(video_paths))]
+    # trackers = [Sort() for _ in range(len(cam_idxs))]
 
-cam_idxs = [1, 2, 3, 4, 5, 6, 7, 8]
-videos_path = "/home/lollo/Documents/python/CV/CVBallTracking/data/fake_basket"
-video_paths = [f"{videos_path}/out{cam_idx}.mp4" for cam_idx in cam_idxs]
-cams = [CameraController(cam_idx) for cam_idx in cam_idxs]
-caps = [cv.VideoCapture(video_paths[idx]) for idx in range(len(video_paths))]
-# trackers = [Sort() for _ in range(len(cam_idxs))]
+    cv.namedWindow("frame", cv.WINDOW_NORMAL)
 
+    curr_cam_idx = 4
+    frame_skip = 10
+    frame_idx = 200
 
-cv.namedWindow("frame", cv.WINDOW_NORMAL)
+    steps = from_file()
 
-curr_cam_idx = 4
-frame_skip = 10
-frame_idx = 200
+    plot_points = []
+    for step in steps[500:2000]:
+        frm_idx, all_dets = step
 
+        final_point = detections_to_point(all_dets)
+        print(f"FINAL POINT: {final_point}")
+        if final_point is not None:
+            plot_points.append(final_point)
 
-steps = []
-new_step = {}
-frame_idx = -1
-while True:
+    from mpl_toolkits.mplot3d import Axes3D
 
-    step = dump.pop(0)
-    frm_idx, cam_idx, x, y = step
-    if frame_idx != frm_idx:
-        steps.append([frame_idx, new_step])
-        frame_idx = frm_idx
-        new_step = {}
-    new_step[cam_idx] = [x, y]
-    if len(dump) == 0:
-        steps.append([frame_idx, new_step])
-        steps.pop(0)
-        break
+    plot_points = np.array(plot_points).T
 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.set_box_aspect([1, 1, 1])
+    # plt.ion()
+    # plt.show()
+    # Plotting real_corners
+    ax.scatter(
+        field_corners[:, 0],
+        field_corners[:, 1],
+        field_corners[:, 2],
+        c="red",
+        label="Real Corners",
+    )
+    set_axes_equal(ax)
 
-plot_points = []
-for step in steps[500:2000]:
-    frm_idx, all_dets = step
-
-    final_point = detections_to_point(all_dets)
-    print(f"FINAL POINT: {final_point}")
-    if final_point is not None:
-        plot_points.append(final_point)
-
-from mpl_toolkits.mplot3d import Axes3D
-
-
-plot_points = np.array(plot_points).T
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.set_box_aspect([1, 1, 1])
-# plt.ion()
-# plt.show()
-# Plotting real_corners
-ax.scatter(
-    field_corners[:, 0],
-    field_corners[:, 1],
-    field_corners[:, 2],
-    c="red",
-    label="Real Corners",
-)
-set_axes_equal(ax)
-
-
-ax.plot(plot_points[0], plot_points[1], plot_points[2])
-ax.scatter(*plot_points.T[0], color="red")
-plt.show()
+    ax.plot(plot_points[0], plot_points[1], plot_points[2])
+    ax.scatter(*plot_points.T[0], color="red")
+    plt.show()
--- multi_track.py
+++ multi_track.py
@@ -14,7 +14,8 @@ from common import set_axes_equal
 
 from camera_controller import CameraController
 from yolotools.sliced_yolo import SlicedYOLO
-
+from build_map import from_file
+from kalman import KalmanTracker
 
 positions_path = "data/camera_data/camera_positions.json"
 with open(positions_path, "r") as file:
@@ -22,19 +23,19 @@ with open(positions_path, "r") as file:
     data = json.load(file)
     positions = data["positions"]
     field_corners = np.array(data["field_corners"]) * 1000
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.set_box_aspect([1, 1, 1])
-plt.ion()
-plt.show()
-ax.scatter(
-    field_corners[:, 0],
-    field_corners[:, 1],
-    field_corners[:, 2],
-    c="red",
-    label="Real Corners",
-)
-set_axes_equal(ax)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection="3d")
+# ax.set_box_aspect([1, 1, 1])
+# plt.ion()
+# plt.show()
+# ax.scatter(
+#     field_corners[:, 0],
+#     field_corners[:, 1],
+#     field_corners[:, 2],
+#     c="red",
+#     label="Real Corners",
+# )
+# set_axes_equal(ax)
 
 
 def plot_3d_points(points):
@@ -62,13 +63,15 @@ video_paths = [f"{videos_path}/out{cam_idx}.mp4" for cam_idx in cam_idxs]
 cams = [CameraController(cam_idx) for cam_idx in cam_idxs]
 caps = [cv.VideoCapture(video_paths[idx]) for idx in range(len(video_paths))]
 # trackers = [Sort() for _ in range(len(cam_idxs))]
-
+tracker = Sort()
+kalm = KalmanTracker()
 
 cv.namedWindow("frame", cv.WINDOW_NORMAL)
 
 
 frame_skip = 10
-START = 200
+START = 20
+END = 900
 frame_idx = START
 if START > 0:
     for cap in caps:
@@ -76,46 +79,63 @@ if START > 0:
 
 
 tracked_points = []
+kalman_points = []
 every_det = []
+
+FROMFILE = True
+if FROMFILE:
+    steps = from_file()
+    steps = steps[START:END]
+
 while True:
     print(f"FRAME {frame_idx}-------------------------------")
     all_frames = []
     all_dets = {}
     ret = True
 
-    for curr_cam_idx in range(len(cam_idxs)):
-        cap = caps[curr_cam_idx]
-        cam = cams[curr_cam_idx]
+    if not FROMFILE:
+        for curr_cam_idx in range(len(cam_idxs)):
+            cap = caps[curr_cam_idx]
+            cam = cams[curr_cam_idx]
 
-        ret, frame = cap.read()
-        if not ret:
-            print("[TRACK] Frame corrupted, exiting...")
-            exit()
+            ret, frame = cap.read()
+            if not ret:
+                print("[TRACK] Frame corrupted, exiting...")
+                exit()
 
-        uframe = frame
+            uframe = frame
 
-        out, det, uframe = sliced_yolo.predict(uframe, viz=True)
+            out, det, uframe = sliced_yolo.predict(uframe, viz=True)
 
-        if out is not None:
-            x, y, w, h, c = out
-            all_dets[curr_cam_idx] = [x + w // 2, y + h // 2]
-        all_frames.append(cv.resize(uframe, (640, 480)))
-
-    frame_idx = cap.get(cv.CAP_PROP_POS_FRAMES)
+            if out is not None:
+                x, y, w, h, c = out
+                all_dets[curr_cam_idx] = [x + w // 2, y + h // 2]
+            all_frames.append(cv.resize(uframe, (640, 480)))
+            frame_idx = cap.get(cv.CAP_PROP_POS_FRAMES)
+    else:
+        if len(steps) == 0:
+            break
+        step = steps.pop(0)
+        frame_idx, all_dets = step
 
     final_point = CameraController.detections_to_point(all_dets, cams)
+    # tracker.update(final_point)
 
     if final_point is None:
         continue
 
-    print(f"FINAL POINT: {final_point}")
-
-    # tracked_points.append(final_point)
+    # print(f"FINAL POINT: {final_point}")
+    tp = kalm.update(final_point)
+    kalm_point = np.array([tp[0][0], tp[1][0], tp[2][0]])
+    # print(f"KALMAN POINT: {kalm_point}")
+    kalman_points.append(kalm_point)
+    tracked_points.append(final_point)
     # tracked_points_np = np.array(tracked_points)
     # plot_3d_points(tracked_points_np)
 
-    frame = np.vstack([np.hstack(all_frames[:4]), np.hstack(all_frames[4:])])
-    cv.imshow("frame", frame)
+    if not FROMFILE:
+        frame = np.vstack([np.hstack(all_frames[:4]), np.hstack(all_frames[4:])])
+        cv.imshow("frame", frame)
     k = cv.waitKey(1)
     if k == ord("d"):
         print(f"SKIPPING {frame_skip} FRAMES")
@@ -123,3 +143,33 @@ while True:
     if k == ord("q"):
         print("EXITING")
         break
+
+tracked_points_np = np.array(tracked_points)
+kalman_points_np = np.array(kalman_points)
+
+plot_points = np.array(tracked_points_np).T
+kalman_plot_points = np.array(kalman_points_np).T
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.set_box_aspect([1, 1, 1])
+# plt.ion()
+# plt.show()
+# Plotting real_corners
+ax.scatter(
+    field_corners[:, 0],
+    field_corners[:, 1],
+    field_corners[:, 2],
+    c="red",
+    label="Real Corners",
+)
+set_axes_equal(ax)
+
+print(plot_points - kalman_plot_points)
+ax.plot(plot_points[0], plot_points[1], plot_points[2], color="blue")
+ax.scatter(*plot_points.T[0], color="red")
+ax.plot(
+    kalman_plot_points[0], kalman_plot_points[1], kalman_plot_points[2], color="green"
+)
+ax.scatter(*kalman_plot_points.T[0], color="green")
+plt.show()
