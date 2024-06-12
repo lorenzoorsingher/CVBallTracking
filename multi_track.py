238d9a76aced2b9abfb8ef157697f77cc7231eda
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-06-12 20:21:28 +0200

    remove kalman filter from track

--- multi_track.py
+++ multi_track.py
@@ -62,16 +62,14 @@ videos_path = "/home/lollo/Documents/python/CV/CVBallTracking/data/fake_basket"
 video_paths = [f"{videos_path}/out{cam_idx}.mp4" for cam_idx in cam_idxs]
 cams = [CameraController(cam_idx) for cam_idx in cam_idxs]
 caps = [cv.VideoCapture(video_paths[idx]) for idx in range(len(video_paths))]
-# trackers = [Sort() for _ in range(len(cam_idxs))]
-tracker = Sort()
-kalm = KalmanTracker()
+
 
 cv.namedWindow("frame", cv.WINDOW_NORMAL)
 
 
 frame_skip = 10
-START = 20
-END = 900
+START = 750
+END = 1000
 frame_idx = START
 if START > 0:
     for cap in caps:
@@ -79,14 +77,13 @@ if START > 0:
 
 
 tracked_points = []
-kalman_points = []
 every_det = []
 
-FROMFILE = True
+FROMFILE = False
 if FROMFILE:
     steps = from_file()
     steps = steps[START:END]
-
+final_point = None
 while True:
     print(f"FRAME {frame_idx}-------------------------------")
     all_frames = []
@@ -118,20 +115,12 @@ while True:
         step = steps.pop(0)
         frame_idx, all_dets = step
 
-    final_point = CameraController.detections_to_point(all_dets, cams)
-    # tracker.update(final_point)
+    final_point = CameraController.detections_to_point(all_dets, cams, final_point)
 
     if final_point is None:
         continue
 
-    # print(f"FINAL POINT: {final_point}")
-    tp = kalm.update(final_point)
-    kalm_point = np.array([tp[0][0], tp[1][0], tp[2][0]])
-    # print(f"KALMAN POINT: {kalm_point}")
-    kalman_points.append(kalm_point)
     tracked_points.append(final_point)
-    # tracked_points_np = np.array(tracked_points)
-    # plot_3d_points(tracked_points_np)
 
     if not FROMFILE:
         frame = np.vstack([np.hstack(all_frames[:4]), np.hstack(all_frames[4:])])
@@ -145,17 +134,13 @@ while True:
         break
 
 tracked_points_np = np.array(tracked_points)
-kalman_points_np = np.array(kalman_points)
 
 plot_points = np.array(tracked_points_np).T
-kalman_plot_points = np.array(kalman_points_np).T
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 ax.set_box_aspect([1, 1, 1])
-# plt.ion()
-# plt.show()
-# Plotting real_corners
+
 ax.scatter(
     field_corners[:, 0],
     field_corners[:, 1],
@@ -165,11 +150,6 @@ ax.scatter(
 )
 set_axes_equal(ax)
 
-print(plot_points - kalman_plot_points)
 ax.plot(plot_points[0], plot_points[1], plot_points[2], color="blue")
-ax.scatter(*plot_points.T[0], color="red")
-ax.plot(
-    kalman_plot_points[0], kalman_plot_points[1], kalman_plot_points[2], color="green"
-)
-ax.scatter(*kalman_plot_points.T[0], color="green")
+ax.scatter(*plot_points.T[0], color="blue")
 plt.show()
